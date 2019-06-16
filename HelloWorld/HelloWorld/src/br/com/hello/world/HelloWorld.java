ccc80a1ca7cd68be1e17fb672a195738a937fdbe
Jeferson <jg_jeferson@hotmail.com>
2019-06-16 00:32:27 -0300

    ok

---
+++ HelloWorld/HelloWorld/src/br/com/hello/world/HelloWorld.java
@@ -0,0 +1,14 @@
+package br.com.hello.world;
+
+/**
+ *
+ * @author Jeferson Luis
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, world!\n");
+        System.out.println("Oi, mundo!\n");
+    }
+
+}
