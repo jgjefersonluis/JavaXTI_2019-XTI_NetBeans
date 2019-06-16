f5d3d510f97e168e635a96fa724f057c134833a5
Jeferson <jg_jeferson@hotmail.com>
2019-06-16 01:10:28 -0300

    ok

---
+++ aula05/HelloWorld/src/HelloWorldJavaSE.java
@@ -0,0 +1,16 @@
+/**
+ * Programa que imprime texto na tela.
+ *
+ * @author jg_jeferson
+ */
+public class HelloWorldJavaSE {
+
+    /* Metodo principal da classe */
+    public static void main(String[] args) {
+        System.out.println("\tOi, mundo!"); // caracteres de escape
+        System.out.println("Oi\", mundo!");
+        System.out.println("Java \\SE");
+        System.out.println("Java \nSE");
+    } // fim do metodo
+
+} // fim da classe ou programa
